279f403b7c7532b1b20d447991690f0989d286ca
Harrison Zhao <harrisonzhao97@gmail.com>
2022-08-02 17:18:01 -0700

    refactor: changed stress tests to use scenario

--- stress_tests/reviews.js
+++ stress_tests/reviews.js
@@ -5,20 +5,28 @@ import { Counter } from 'k6/metrics';
 export const requests = new Counter('http_reqs');
 
 export const options = {
-    stages: [
-      { target: 1, duration: '20s' },
-      { target: 10, duration: '0s'},
-      { target: 10, duration: '20s' },
-      { target: 100, duration: '0s' },
-      { target: 100, duration: '20s' },
-      { target: 1000, duration: '0s' },
-      { target: 1000, duration: '20s'},
-    ]
-    // ,
-    // thresholds: {
-    //   http_req_duration: ['avg<50'],
-    // }
-  // }
+    // stages: [
+    //   { target: 1, duration: '20s' },
+    //   { target: 10, duration: '0s'},
+    //   { target: 10, duration: '20s' },
+    //   { target: 100, duration: '0s' },
+    //   { target: 100, duration: '20s' },
+    //   { target: 1000, duration: '0s' },
+    //   { target: 1000, duration: '20s'},
+    // ],
+    scenarios: {
+      constant_request_rate: {
+        executor: 'constant-arrival-rate',
+        rate: 1000,
+        timeUnit: '1s',
+        duration: '30s',
+        preAllocatedVUs: 1000,
+        maxVUs: 2000
+      }
+    },
+    thresholds: {
+      http_req_duration: ['avg<50'],
+    }
 };
 
 export default function reviewsStressTest() {
