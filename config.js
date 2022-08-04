1bc42a0957515288372fef8902d222fe4e831f6b
Harrison Zhao <harrisonzhao97@gmail.com>
2022-08-03 17:19:34 -0700

    refactor: config.js for deployment

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-const url = '127.0.0.1'
-
+const url = '127.0.0.1';
 const PORT = 3000;
+const PGUSER = 'ubuntu';
+const PGPASSWORD = 'qwe';
+const PGHOST = 'ec2-54-215-185-133.us-west-1.compute.amazonaws.com';
+const PGDATABASE = 'reviews';
+const PGPORT = 5432;
 
-module.exports = {url, PORT};
+module.exports = {url, PORT, PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT};
