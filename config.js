6b9835f56abfb3cb23ac15d63ac1bea3d3e75bbd
Harrison Zhao <harrisonzhao97@gmail.com>
2022-08-04 10:54:26 -0700

    feat: loader.io route added

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const url = '127.0.0.1';
 const PORT = 3000;
 const PGUSER = 'ubuntu';
 const PGPASSWORD = 'qwe';
-const PGHOST = 'ec2-54-215-185-133.us-west-1.compute.amazonaws.com';
+const PGHOST = 'ec2-52-8-58-49.us-west-1.compute.amazonaws.com';
 const PGDATABASE = 'reviews';
 const PGPORT = 5432;
 
--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ app.post('/reviews', controllers.reviews.add);
 app.get('/reviews/meta', controllers.metadata.get);
 app.put('/reviews/:review_id/helpful', controllers.update.helpful);
 app.put('/reviews/:review_id/report', controllers.update.report);
+app.get('/loaderio-11ee4a0994d7e0bde1308b3659477209', (req, res) =>
+  res.send('loaderio-11ee4a0994d7e0bde1308b3659477209'));
 
 app.listen(PORT);
 console.log(`Server listening at http://localhost:${PORT}`);
